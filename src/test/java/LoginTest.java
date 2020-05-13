a5460fc89216b4562061c97b291ca755401fd95f
Nithin <nithraj7@gmail.com>
2020-05-13 19:35:53 +0530

    Initial Commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+	
+	
+	public static WebDriver driver;
+	
+	
+	@BeforeSuite
+	public static void setUp() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		
+	}
+
+	@Test
+	public void doLogin() {
+		
+		
+		driver.get("http://gmail.com");
+		
+		driver.findElement(By.id("identifierId")).sendKeys("nithraj7");
+		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/span/span")).click();
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		
+		driver.quit();
+	}
+}
